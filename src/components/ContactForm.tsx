19e122da36089a02d4fbdd0bbd760c3e61fe9922
Blanco237 <asongrandy9@gmail.com>
2024-08-01 15:07:26 +0100

    Fetching contacts for contact list

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,35 +1,73 @@
-import { useState } from 'react';
+"use client";
+
+import { useState } from "react";
+import { Form, Input, Button, message, Table, Popconfirm } from "antd"; // Import Ant Design components
 
 const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     // Replace with your actual contact saving logic
-    await fetch('/api/contacts', {
-      method: 'POST',
+    await fetch("/api/contacts", {
+      method: "POST",
       body: JSON.stringify({ name, phoneNumber }),
     });
     // Handle success or error
+    message.success("Contact added successfully!");
+    setName("");
+    setPhoneNumber("");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Enter Name"
-      />
-      <input
-        type="text"
-        value={phoneNumber}
-        onChange={(e) => setPhoneNumber(e.target.value)}
-        placeholder="Enter Phone Number"
-      />
-      <button type="submit">Add Contact</button>
-    </form>
+    <div className="flex flex-col px-4 pt-20 items-center justify-center min-h-[50vh] bg-gray-100">
+      <h2 className="text-2xl font-bold mb-4">Add Contact</h2>
+      <Form
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={handleSubmit}
+        autoComplete="off"
+        className="w-full max-w-md"
+        layout="vertical"
+      >
+        <Form.Item
+          label="Name"
+          name="name"
+          rules={[{ required: true, message: "Please input your name!" }]}
+        >
+          <Input
+            type="text"
+            size="large"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter Name"
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="Phone Number"
+          name="phoneNumber"
+          rules={[
+            { required: true, message: "Please input your phone number!" },
+          ]}
+        >
+          <Input
+            type="text"
+            size="large"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            placeholder="Enter Phone Number"
+          />
+        </Form.Item>
+
+        <Form.Item className="w-full justify-center flex">
+          <Button type="primary" htmlType="submit">
+            Add Contact
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -1,26 +1,69 @@
+'use client';
+
 import { Contact } from '@/types';
+import { fetchContacts } from '@/utils/actions';
+import { Button, Popconfirm, Table } from 'antd';
 import { useState, useEffect } from 'react';
 
 const ContactList = () => {
   const [contacts, setContacts] = useState<Array<Contact>>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchContacts = async () => {
-      const response = await fetch('/api/contacts');
-      const data = await response.json();
-      setContacts(data);
-    };
-    fetchContacts();
+    const load = async () => {
+      setLoading(true)
+      const conts = await fetchContacts();
+      setContacts(conts)
+      setLoading(false);
+    }
+
+    load();
   }, []);
 
+  const handleDelete = async (id: string) => {
+    await fetch(`/api/contacts/${id}`, {
+      method: 'DELETE',
+    });
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  };
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Phone Number',
+      dataIndex: 'phone',
+      key: 'phone',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (record: Contact) => (
+        <Popconfirm
+          title="Are you sure delete this contact?"
+          onConfirm={() => handleDelete(record.id)}
+          onCancel={() => {}}
+          okText="Yes"
+          cancelText="No"
+        >
+          <Button type="default" danger >Delete</Button>
+        </Popconfirm>
+      ),
+    },
+  ];
+
   return (
-    <ul>
-      {contacts.map((contact) => (
-        <li key={contact.id}>
-          {contact.name} - {contact.phone}
-        </li>
-      ))}
-    </ul>
+
+    <Table
+      columns={columns}
+      dataSource={contacts}
+      pagination={false}
+      rowKey="id"
+      loading={loading}
+    />
   );
 };
 
